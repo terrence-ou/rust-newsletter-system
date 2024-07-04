5ba6b5ac94f3e5e48362a66498c3972fb4cc6f1b
terrence-ou <outerrencedl@gmail.com>
2024-07-04 15:07:21 -0400

    port setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use rust_newsletter_system::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind the port");
+    run(listener)?.await
 }
