1c6019d1f2e38e21750a2811ab0f58a9f28a6b31
terrence-ou <outerrencedl@gmail.com>
2024-07-05 16:24:10 -0400

    file structure updated

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use rust_newsletter_system::run;
+use rust_newsletter_system::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind the port");
+    println!("http://127.0.0.1:{}", listener.local_addr().unwrap().port());
     run(listener)?.await
 }
