cb719560db4a0180e73a2fd484611f8b786f49f5
terrence-ou <outerrencedl@gmail.com>
2024-07-13 18:33:38 -0400

    database configuration updated

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use rust_newsletter_system::configuration::get_configuration;
 use rust_newsletter_system::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind the port");
-    println!("http://127.0.0.1:{}", listener.local_addr().unwrap().port());
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
